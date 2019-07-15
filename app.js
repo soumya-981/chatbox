d4578c29f9ab6181915b351f211d2f8280d9c249
soumya-981 <soumyajitsur602@gmail.com>
2019-07-16 00:03:46 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+   res.sendfile('index.html');
+});
+
+users = [];
+io.on('connection', function(socket) {
+   console.log('A user connected');
+   socket.on('setUsername', function(data) {
+      console.log(data);
+      
+      if(users.indexOf(data) > -1) {
+         socket.emit('userExists', data + ' username is already exist! Try others');
+      } else {
+         users.push(data);
+         socket.emit('userSet', {username: data});
+      }
+   });
+   
+   socket.on('msg', function(data) {
+      //Send message to every connected user
+      io.sockets.emit('newmsg', data);
+   })
+});
+
+http.listen(3000, function() {
+   console.log('listening on localhost:3000');
+});
